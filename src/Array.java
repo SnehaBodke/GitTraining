01516f329e36e04efc62429969fdbce1dc75f08c
Sneha <Snehabodke@yahoo.com>
2022-12-13 11:35:32 +0000

    First commit

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,23 @@
 public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      int [] employee=new int [4];
+		
+		employee[0]=1200;
+		employee[1]=1300;
+		employee[2]=1400;
+	    employee[3]=1500;
+	    
+	    System.out.println(employee[0]);
+	    System.out.println(employee[1]);
+	    System.out.println(employee[2]);
+	    System.out.println(employee[3]);
+	    
+	    for(int i=0;i<=3;i++) 
+	    {
+	       System.out.println(employee[i]);	
+	    }
+	    
 
 	}
 
